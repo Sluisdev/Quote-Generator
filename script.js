75caa921b7bad945c1effcd28161fa2082673ac3
Sluisdev <152731265+Sluisdev@users.noreply.github.com>
2025-11-08 13:04:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 // write your JavaScript here
 
 const quoteParagr = document.querySelector('.quote')
+const authorElement = document.querySelector('.author')
 const twitterBtn = document.querySelector('.twitter-button')
 const quoteBtn = document.querySelector('.quote-button')
+const quoteBox = quoteParagr.innerText
+// const authrBox = authorElement.innerText
 
-console.dir(quoteParagr)
 
 quoteBtn.addEventListener('click', async (e) => {
    try {
-   const paragrValue = quoteParagr.innerText
-   const randQuote = await axios.get('https://quoterism.com/api/quotes/random')
-   console.log(randQuote)
+   const response = await axios.get('https://corsproxy.io/?https://quoterism.com/api/quotes/random')
+   const quote = response.data.text;
+   const {name} = response.data.author
+   quoteParagr.innerText = quote
+   authorElement.innerText = name
    } catch (e) {
       console.log('ERROR:', e)
    }
    
 })
 
+// twitterBtn.addEventListener('click', (e) => {
 
-
-// fetch('https://quoterism.com/api/quotes/random'), {
-//   mode:  'cors', 
-//   method: 'GET',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// }
-
-// const api_url ="https://zenquotes.io/api/quotes/";
-
-// async function getapi(url)
-// {
-//   const response = await fetch(url);
-//   var data = await response.json();
-//   console.log(data);
-// }
-
-
+// })
